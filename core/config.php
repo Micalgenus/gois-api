f85298e21c2649c9d5e532ae95d29497bc70d63e
Micalgenus <micalgenus@gmail.com>
2016-11-06 00:34:09 +0900

    결과 반환용 함수
    response = http code를 포함하여 반환 후 종료,
    json_return = 결과를 json 형태로 반환

--- core/config.php
+++ core/config.php
@@ -6,4 +6,32 @@ $db_config = dirname(__FILE__) . '/../config/db.config';
 // Router Config File Path
 $router_config = dirname(__FILE__) . '/../config/router.config';
 
+
+/////////////////////
+// Global function //
+/////////////////////
+
+/**
+ * @brief 실행 결과 반환
+ */
+function response($status, $msg) {
+  echo $msg;
+  http_response_code($status);
+  exit;
+}
+
+/**
+ * @brief 결과를 JSON데이터로 반환
+ */
+function json_return($status = NULL, $obj = NULL) {
+  if ($status == NULL) {
+    response(500, "JSON Return Error");
+  }
+
+  $result = $obj;
+  $result->status = $status;
+
+  response(200, json_encode($result));
+}
+
 ?>
