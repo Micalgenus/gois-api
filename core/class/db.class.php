548157fe93165d9b159e6ad2d7b0a0a163737226
Micalgenus <micalgenus@gmail.com>
2016-11-06 00:36:38 +0900

    주석 정리
    파일 내 주석 정리

--- core/class/db.class.php
+++ core/class/db.class.php
@@ -1,6 +1,12 @@
 <?php
 /**
  * @Class MySQL Database Manager
+ *
+ * @hostname = 서버 주소
+ * @database = 데이터베이스 이름
+ * @username = 계정 이름
+ * @password = 계정 비밀번호
+ * @conn = 디비 주체 변수
  */
 class DB {
   private $hostname;
@@ -12,6 +18,8 @@ class DB {
 
   /**
    * @brief DB config파일을 읽고, DB연동 설정 변수를 적용
+   *
+   * @config_file = 설정 파일의 경로 (string)
    */
   function __construct() {
     // Config 파일을 JSON으로 읽어들임
@@ -24,13 +32,16 @@ class DB {
     $this->password = $config['password'];
   }
 
+  /**
+   * @brief DB 연결 시행
+   */
   function Connection() {
-    // DB 연동
     try {
+      // DB 연동
       $this->conn = new PDO("mysql:host=$this->hostname;dbname=$this->database", $this->username, $this->password);
       $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
-      // Error 발생
+      // DB Error 예외처리
       //echo 'Connection failed: ' . $e->getMessage();
       echo 'Database Error';
 
--- core/class/router.class.php
+++ core/class/router.class.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * @Class Routing Manager
+ *
+ * @subject = 라우팅 기준의 최상위 값 
+ * @command = 라우팅 기준의 2번째 값
+ * @option = 라우팅 기준의 3번째 값
+ * @routing = 라우팅 설정 파일의 내용 값
  */
 class Router {
   private $subject;
@@ -11,6 +16,8 @@ class Router {
 
   /**
    * @brief config파일을 불러오고, URL을 명령으로 정리함
+   *
+   * @config_file = 설정 파일의 경로 (string)
    */
   function __construct() {
     // Config 파일을 JSON으로 읽어들임
@@ -29,6 +36,12 @@ class Router {
     $this->option = strtoupper($this->option);
   }
 
+  /**
+   * @brief 객체 내 변수를 이용하여 라우팅할 문자열 구함
+   *
+   * @return 라우팅이 불가능 할 경우 NULL
+   *         가능할 경우 파일의 경로를 반환
+   */
   function getRoutingString() {
     // routing 할 값이 없음
     if ($this->subject == NULL) {
@@ -51,6 +64,11 @@ class Router {
   }
 
   function configCheck($value) {
+  /**
+   * @brief 인자로 온 값이 config파일에서 리프노드 인지 확인
+   *
+   * @value 확인할 값
+   */
     // 들어온 값이 문자열로 존재하는지 확인
     // array일 경우 하위 메뉴가 있음, 정보가 부족함
     if (gettype($value) == 'string') {
