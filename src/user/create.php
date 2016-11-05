8b1e44a17e5e2d81067d3e15df434254affcd1c3
Micalgenus <micalgenus@gmail.com>
2016-11-06 00:35:02 +0900

    user create
    Test용 폼

--- src/user/create.php
+++ src/user/create.php
@@ -1,3 +1,10 @@
 <?php
-echo "Test";
+echo $_POST['admin_id'];
+echo $_POST['admin_pw'];
 ?>
+
+<form action="https://api.gois.me/user/create" method="post">
+  <input type="text" name="admin_id" />
+  <input type="text" name="admin_pw" />
+  <input type="submit" />
+</form>
